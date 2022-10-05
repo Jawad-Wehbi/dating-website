bea4b4cb6c109d02e2ca7aaf584260e92589e188
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-05 22:41:34 +0300

    Backend<> log-in Implementation

--- Backend-datingWebsite/app/Http/Controllers/AuthController.php
+++ Backend-datingWebsite/app/Http/Controllers/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Validation\Factory;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
+use Auth;
+use Validator;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {   
@@ -33,7 +34,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'username' => $request->username,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => Hash::make($request->password),
             'bio' => $request->bio,
             'profile_photo_URL' => $request->profile_photo_URL,
             'gender' => $request->gender,
@@ -47,7 +48,31 @@ class AuthController extends Controller
             'user'=>$user
         ],201);
     }
-    public function login(Request $request){
-        
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 120
+        ]);
     }
 };
+
